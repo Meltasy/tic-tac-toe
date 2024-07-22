76e17faf130fbbfa0d279ec9f31439d97b8601a9
Meltasy <119603631+Meltasy@users.noreply.github.com>
2024-07-22 12:03:49 +0200

    Update js and html files and create css file
    Update html file to include gameboard grid
    Create css file to style gameboard
    Update functions in js to ensure every eventuality

--- script.js
+++ script.js
@@ -2,51 +2,37 @@ const createPlayer = (name, symbol) => {
     return {
         name: name,
         symbol: symbol,
-        // colour: colour,
     };
 };
-let playerOne = createPlayer("Melissa", "X");
-let playerTwo = createPlayer("Guillaume", "O");
+
+const players = [
+    createPlayer("Melissa", "X"),
+    createPlayer("Guillaume", "O"),
+]
 
 const gameboard = (function () {
     const board = [];
     for (i = 0; i < 9; i++) {
         board.push(0);
     };
-    console.log(board);
-    return { board };
-})();
-
-const playGame = (function () {
+    function resetBoard() {
+        for (i = 0; i < board.length; i++) {
+            board[i] = 0;
+        };
+    };
     function getCurrentPlayer() {
         let emptyCells = 0;
-        gameboard.board.forEach(checkValue);
-        function checkValue(item) {
+        board.forEach(function(item) {
             if (item === 0) {
                 emptyCells += 1;
-            }
-        };
-        if (emptyCells % 2 !== 0) {
-            currentPlayer = playerOne;
-        } else {
-            currentPlayer = playerTwo;
-        };
-    }
-    function playRound() {
-        getCurrentPlayer();
-        let cellNum = Number(window.prompt(`${this.currentPlayer.name}, Which cell number do you want?`, ""));
-        if (gameboard.board[cellNum] === 0 && cellNum !== 9) {
-            if (this.currentPlayer === playerOne) {
-                gameboard.board.splice(cellNum, 1, "X");
-            } else if (this.currentPlayer === playerTwo) {
-                gameboard.board.splice(cellNum, 1, "O");
             };
-            console.log(cellNum);
-            console.log(gameboard.board);
-        }
-        
-    }
-    console.log(playRound());
+        });
+        if (emptyCells % 2 !== 0) {
+            return 0;
+        } else {
+            return 1;
+        };
+    };
     const winningCombos = [
         [0, 1, 2],
         [3, 4, 5],
@@ -60,20 +46,75 @@ const playGame = (function () {
     const checkWinner = () => {
         for(let i = 0; i < winningCombos.length; i++) {
             const [a, b, c] = winningCombos[i];
-            if (gameboard.board[a] === gameboard.board[b] && gameboard.board[b] === gameboard.board[c] && gameboard.board[c] === "X") {
-                return alert(`${playerOne.name} is the winner!`);
-            } else if (gameboard.board[a] === gameboard.board[b] && gameboard.board[b] === gameboard.board[c] && gameboard.board[c] === "O") {
-                return alert(`${playerTwo.name} is the winner!`);
-            // } else if (gameboard.board.forEach(item => item !== 0)) {
-            //     return alert("It's a tie!")
-            } else {
-                playRound();
+            if (board[a] === board[b] && board[b] === board[c] && board[c] === players[0].symbol) {
+                return 0;
+            } else if (board[a] === board[b] && board[b] === board[c] && board[c] === players[1].symbol) {
+                return 1;
             };
-            // Why does this not work?
-            if (gameboard.board.forEach(item => item !== 0)) {
-                return alert("It's a tie!")
-            }
+        };
+        checkTie();
+    };
+    const checkTie = () => {
+        let fullCells = 0;
+        board.forEach(function(item) {
+            if (item !== 0) {
+                fullCells += 1;
+            };
+        });
+        if (fullCells === 9) {
+            return 2;
         };
     };
-    console.log(checkWinner());
+    return { board, resetBoard, getCurrentPlayer, checkWinner, checkTie };
 })();
+
+const playGame = (function () {
+    function startGame() {
+        const startBtn = document.querySelector(".start");
+        startBtn.addEventListener("click", (e) => {
+            gameboard.resetBoard();
+            updateDisplay();
+        });
+    }
+    startGame();
+    function playRound(cellNum) {
+        if (gameboard.board[cellNum] === 0) {
+            gameboard.board.splice(cellNum, 1, players[gameboard.getCurrentPlayer()].symbol);
+            gameboard.checkWinner();
+        };
+        console.log(gameboard.board);
+    };
+    return { playRound }
+})();
+
+const updateDisplay = function() {
+    let game = document.querySelector("#gameboard");
+    while (game.firstChild) {
+        game.removeChild(game.lastChild);
+    };
+    for (i = 0; i < gameboard.board.length; i++) {
+        createGameboard(gameboard.board[i], i);
+    };
+    function createGameboard(cellValue, cellNum) {
+        let cell = document.createElement("button");
+        cell.textContent = `${cellValue}`;
+        cell.addEventListener("click", (e) => {
+            playGame.playRound(cellNum);
+            updateDisplay();
+            if (gameboard.checkWinner() === 0) {
+                alert(`${players[0].name} is the winner!`);
+                gameboard.resetBoard();
+                updateDisplay();
+            } else if (gameboard.checkWinner() === 1) {
+                alert(`${players[1].name} is the winner!`);
+                gameboard.resetBoard();
+                updateDisplay();
+            } else if (gameboard.checkTie() === 2) {
+                alert("It's a tie!");
+                gameboard.resetBoard();
+                updateDisplay();
+            };
+        });
+        game.appendChild(cell);
+    }
+};
